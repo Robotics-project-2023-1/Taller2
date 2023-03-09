a19d09296a287e45dcd0a79f1154880380e2d044
Fernandralo <f.alvarezl@uniandes.edu.co>
2023-03-09 11:29:14 -0500

    Nodo robot_teleop

---
+++ mi_robot_2/src/build/mi_robot_2/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/robotica/mi_robot_2/src/install/mi_robot_2'
---
+++ mi_robot_2/src/mi_robot_2/mi_robot_2/robot_teleop.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from mi_robot_2.')
+
+
+if __name__ == '__main__':
+    main()
