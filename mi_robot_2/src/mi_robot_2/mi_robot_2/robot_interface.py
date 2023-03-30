bd67bbdd565ce85da2b2f49f0f0d14397c220973
santyxd3000 <s.pardom@uniandes.edu.co>
2023-03-29 19:00:13 -0500

    Cambios grandes

--- mi_robot_2/src/mi_robot_2/mi_robot_2/robot_interface.py
+++ mi_robot_2/src/mi_robot_2/mi_robot_2/robot_interface.py
@@ -1,145 +1,217 @@
-import tkinter as tk
+import tkinter as tk #libreria para crear la interfaz
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import Canvas
 from PIL import Image, ImageTk, ImageGrab
 import pyautogui
 from pynput import keyboard
-
-root = tk.Tk()
-root.geometry('750x600')
-root.title('robot_teleop')
-root.configure(bg='#ff3333')
-img = Image.open("cuadricula.png")
-imagen = img.resize((400,400))
-new_image = ImageTk.PhotoImage(imagen)
-etiqueta_imagen = tk.Label(root,image=new_image)
-
-ancho = 750
-alto = 600
-pos_x = 400
-pos_y = 300
-
-canvas = Canvas(root, width = ancho, height = alto)
-canvas.configure(bg = '#ff3333')
-canvas.pack()
-canvas.create_image(pos_x,pos_y,image=new_image)
-
-av = 3
-x1 = pos_x
-y1 = pos_y
-x2 = pos_x +5
-y2 = pos_y +5
-
-text_frame_grafica = tk.Text(root, height=1, width=25,font=("Futura", 20))
-text_frame_grafica.place(x=350,y=30)
-
-label_grafica = tk.Label(root, text="Introduce el nombre:",bg='#ff3333',font=("Futura", 20))
-label_grafica.place(x=0,y=32)
-
-def open_file():
-    file_path = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All Files", "*.*")))
-    try:
-        global image
-        image = Image.open(file_path)
-        image = image.resize((400,400))
-        render = ImageTk.PhotoImage(image)
-        img = tk.Label(root, image=render)
-        img.image = render
-        # img.place(x=100,y=150)
-        canvas.create_image(pos_x,pos_y,image=render)
-    except:
-        messagebox.showerror("Error", "Failed to open the image.")
-
-open_file_button = tk.Button(root, text="Seleccionar Imagen", command=open_file, height=2, width=20, font=("Futura", 12))
-open_file_button.place(x=30,y=520)
-
-def save_screenshot():
-    x = root.winfo_rootx()
-    y = root.winfo_rooty()
-    width = root.winfo_width()
-    height = root.winfo_height()
-    # # Crea una imagen a partir de la ventana actual
-    # image = ImageGrab.grab((x, y, x + width, y + height))
+import os # para acceder a los archivos de la carpeta
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist    
+from std_msgs.msg import String
+from threading import Thread 
+from time import sleep
+from robot_interfaces.srv import Reproducir 
 
 
-    dx = (width-750)/2
-    # Crea una imagen a partir de la ventana actual
-    image = ImageGrab.grab((x+dx+200, y+100, x+dx+600, y + 500))
-
-    # Guarda la imagen en un archivo
-    texto = str(text_frame_grafica.get("1.0",'end-1c'))
-    file_path = filedialog.asksaveasfilename(defaultextension=".png", initialfile= texto + ".png", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All Files", "*.*")))
-    image.save(file_path)
-
-# Crea el botón
-save_screenshot_button = tk.Button(root, text="Tomar pantalla", command=save_screenshot, height=1, width=15, font=("Futura", 12))
-save_screenshot_button.place(x=30,y=450)
-
-def on_press(key):
-    try:
-        global x1, y1, x2, y2
-        if key.char == "w":
-            y1 += -av
-            y2 += -av
-            canvas.create_rectangle(x1, y1, x2, y2, fill='blue', outline='blue')         
-        if key.char == "s":
-            y1 += av
-            y2 += av
-            canvas.create_rectangle(x1, y1, x2, y2, fill='blue', outline='blue')
-        if key.char == "a":
-            x1 += -av
-            x2 += -av
-            canvas.create_rectangle(x1, y1, x2, y2, fill='blue', outline='blue')
-        if key.char == "d":
-            x1 += av
-            x2 += av
-            canvas.create_rectangle(x1, y1, x2, y2, fill='blue', outline='blue')
-        print('{0} presionada'.format(
-                key.char))
-    except AttributeError:
-        print('{0} presionada'.format(
-            key))
-
-def on_release(key):
-    print('{0}'.format(
-        key))
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
-
-listener = keyboard.Listener(
-    on_press=on_press,
-    on_release=on_release)
-listener.start()
-
-# Run the main loop
-root.mainloop()
-listener.join()
-
-'''
-# Create a new tkinter window
-#ESTO DESPUES TOCA AÑADIRLO A UNA FUNCION
-root = tk.Tk()
-root.geometry('850x600')
-root.configure(bg='#ff3333')
-
-# Set the title of the window
-root.title("robot_interface")
-
-# Create a label widget
-label = tk.Label(root, text="Hello, World!")
-label.pack()
-
-# Create a button widget
-button = tk.Button(root, text="Click me!")
-button.pack()
-
-# Run the main loop
-root.mainloop()
+global quiero_txt
+quiero_txt = False
+nombre_txt = None 
+guardar_movimientos = False
+filename = None
+folder_path = None
+posiciones = [0,0]
+tecla_presionada = '.'
+keys_pressed = []
 
 
-'''
+def creo_interfaz(): #esta funcion corre como un segundo hilo
+    root = tk.Tk()
+    root.geometry('850x600')
+    root.title("_bot_teleop")
+    root.configure(bg='#7aebc5')
+    name = "gato"
+    archivo = os.path.dirname(__file__)
+    f = os.path.expanduser(archivo + '/' + name + '.jpeg')
+    img = Image.open(f) #modificado para encontrar el path de la foto, si no les sirve descomenten la linea de abajo y comenten esto
+    #img = Image.open('gato.jpeg')
+    imagen = img.resize((400,400))
+    new_image = ImageTk.PhotoImage(imagen)
+    etiqueta_imagen = tk.Label(root, image=new_image)
+    etiqueta_imagen.place(x=250,y=110)
+    ancho = 800
+    alto = 600
+    pos_x = 400
+    pos_y = 300
 
+    canvas = Canvas(root, width = ancho, height = alto)
+    canvas.configure(bg='#7aebc5')
+    canvas.pack()
+
+    canvas.create_image(pos_x,pos_y,image=new_image)
+
+    x1 = pos_x
+    y1 = pos_y
+    x2 = pos_x + 5
+    y2 = pos_y + 5
+
+    text_frame_grafica = tk.Text(root, height=1, width=25,font=("Futura", 15))
+    text_frame_grafica.place(x=350,y=26)
+
+    label_grafica = tk.Label(root, text="Introduce el nombre de la imagen:",bg='#7aebc5',font=("Futura", 15))
+    label_grafica.place(x=0,y=26)
+
+    text_frame_archivo = tk.Text(root, height=1, width=25,font=("Futura", 15))
+    text_frame_archivo.place(x=350,y=65)
+
+    label_grafica = tk.Label(root, text="Introduce el nombre del archivo:",bg='#7aebc5',font=("Futura", 15))
+    label_grafica.place(x=0,y=65)
+
+    def open_file(): #si se desea cambiar la imagen de fondo de interfaz 
+        file_path = filedialog.askopenfilename(initialdir = "/", title = "Select file", filetypes = (("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All Files", "*.*")))
+        try:
+            global image
+            image = Image.open(file_path)
+            image = image.resize((400,400))
+            render = ImageTk.PhotoImage(image)
+            img = tk.Label(root, image=render)
+            img.image = render
+            # img.place(x=100,y=150)
+            canvas.create_image(pos_x,pos_y,image=render)
+        except:
+            messagebox.showerror("Error", "Failed to open the image.")
+
+    open_file_button = tk.Button(root, text="Seleccionar Imagen", command=open_file, height=2, width=17, font=("Futura", 11))
+    open_file_button.place(x=10,y=520)
+
+    def save_screenshot(): #si se desea guardar un pantallazo de la interfaz en su estado actual
+        x = root.winfo_rootx()
+        y = root.winfo_rooty()
+        width = root.winfo_width()
+        height = root.winfo_height()
+        
+        # # para ubuntu:
+        # # Crea una imagen a partir de la ventana actual
+        ####### hacer estos pasos en consola para que funcione:
+        # # 1. $ sudo nano /etc/gdm3/custom.conf
+        # # 2. $ WaylandEnable=false
+        # # 3. guardar los cambios
+        # # 4. $ sudo systemctl restart gdm3
+        image = pyautogui.screenshot(region=(x+200, y+100, width-400, height-200))
+        
+        # # para Windows:
+        # Crea una imagen a partir de la ventana actual
+        # dx = (width-750)/2
+        # image = ImageGrab.grab((x+dx+200, y+100, x+dx+600, y + 500))
+
+        # Guarda la imagen en un archivo
+        texto = str(text_frame_grafica.get("1.0",'end-1c'))
+        file_path = filedialog.asksaveasfilename(defaultextension=".png", initialfile= texto + ".png", filetypes=(("PNG files", "*.png"), ("JPEG files", "*.jpg"), ("All Files", "*.*")))
+        image.save(file_path)
     
+    def read_txt(): # al solicitar el servicio de reproducir un archivo
+        global quiero_txt #variable de control para solicitar el servicio
+        global nombre_txt 
+        try:
+            file_path = filedialog.askopenfilename()
+            nombre = os.path.basename(file_path)
+            nombre_txt = file_path #string con el path para acceder al archivo deseado
+            quiero_txt = True    
+        except:
+            quiero_txt = False
+    
+    def save_to_txt(): #en caso de solicitar un txt con las teclas presionadas
+        global guardar_movimientos
+        global filename
+        global folder_path
+        filename = text_frame_archivo.get('1.0',tk.END).strip()
+        folder_path = filedialog.askdirectory()
+        guardar_movimientos = True
+        with open(folder_path + '/' + filename + ".txt", "w") as file:
+            for key in keys_pressed: #keys pressed se ha guardado desde que inicio a correr la interfaz
+                file.write(key + '\n')
+        pass
 
+        # Crear botones
+    save_screenshot_button = tk.Button(root, text="Tomar pantalla", command=save_screenshot, height=2, width=17, font=("Futura", 11))
+    save_screenshot_button.place(x=10,y=450)
+
+    save_text_button = tk.Button(root, text="Guardar movimientos", command = save_to_txt, height=2, width=19, font=("Futura", 11))
+    save_text_button.place(x=620, y=520)
+
+    read_movements_button = tk.Button(root, text="Reproducir movimientos", command = read_txt, height=2, width=19, font=("Futura", 11))
+    read_movements_button.place(x= 620, y = 450)
+
+           
+    while True:
+        print(posiciones) #visualizar por terminal la posicion actual del robot
+        xd = (posiciones[0]+2.27)*88.3+200
+        yd = ((posiciones[1]+2.27)*88.3-402.5)*(-1)+95
+        sleep(0.1) 
+        canvas.create_rectangle(xd, yd, xd+2, yd+2, fill='blue', outline='blue')
+        root.update_idletasks() #estos 2 comandos reemplazan el root.mainloop()
+        root.update()
+        
+
+class Robot_interface(Node):
+    
+    def __init__(self):
+        global quiero_txt
+        super().__init__('robot_interface')
+        self.subscription = self.create_subscription(Twist, 'robot_position', self.listener_callback_posicion, 10) #nodo se suscribe a robot_position
+        self.subscription  # prevent unused variable warning
+        self.subscription = self.create_subscription(String, 'robot_teclas', self.listener_callback_teclas, 10) #nodo se suscribe a robot_teclas
+        self.subscription  # prevent unused variable warning       
+        self.cli = self.create_client(Reproducir, 'reproducir') #crear cliente para el servicio Reproducir
+        print("cliente creado")
+        while not self.cli.wait_for_service(timeout_sec=2.0): #buscar si el servicio esta disponible
+                self.get_logger().info('service not available, waiting again...')
+        
+        
+    def send_request(self, nombre_txt):
+        global quiero_txt
+        if quiero_txt == True:  #Booleano que activa el servicio
+            self.request = Reproducir.Request() #solicitar servicio
+            self.request.nombre = nombre_txt #mandar la ubicacion del archivo como string
+            self.future = self.cli.call_async(self.request)
+            rclpy.spin_until_future_complete(self, self.future)
+            if self.future.result() is not None:
+                response = self.future.result()
+                if response.respuesta:
+                    print("Ejecutando")
+                else:
+                    print("NOUUU")
+            else:
+                self.get_logger().error('Service call failed')
+            quiero_txt = False
+
+    def listener_callback_posicion(self, msg):
+        global quiero_txt
+        global nombre_txt
+        posiciones[0]  = msg.linear.x #datos de posicion actualizandose 
+        posiciones[1] = msg.linear.y
+        if quiero_txt == True:
+            print("voy a mandar request")
+            self.send_request(nombre_txt) #solicitar servicio 
+            quiero_txt = False
+            
+    def listener_callback_teclas(self, msg): # lee las teclas que se presionan en el nodo robot_teleop
+        tecla_presionada = msg.data
+        keys_pressed.append(tecla_presionada) #almacena las teclas en una lista para despues guardarlas en un txt de ser necesario
+        
+        
+
+def main(args=None):
+    
+    rclpy.init(args=args)
+    t = Thread(target=creo_interfaz) #inicia un segundo hilo en el cual va a correr la interfaz
+    t.start() #inicia la interfaz
+    robot_interface = Robot_interface() #inica el nodo
+    rclpy.spin(robot_interface) 
+    t.join()
+    robot_interface.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- mi_robot_2/src/mi_robot_2/mi_robot_2/robot_teleop.py
+++ mi_robot_2/src/mi_robot_2/mi_robot_2/robot_teleop.py
@@ -1,62 +1,78 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from pynput import keyboard
+from geometry_msgs.msg import Twist    
+from pynput import keyboard   #LIbreria para leer teclado
 from pynput.keyboard import Key, Listener
+
+from std_msgs.msg import String
 
 lin = input("Introduzca la velocidad lineal: ")
 ang = input("Introduzca la velocidad angular: ")
 
-class RobotTeleop(Node):
+class Robot_teleop(Node):
 
     def __init__(self):
 
         super().__init__('robot_teleop')
-        self.publisher_ = self.create_publisher(Twist, 'robot_cmdVel', 10)
-        timer_period = 0.5
+        self.publisher_1 = self.create_publisher(Twist, 'robot_cmdVel', 10)  # 
+        self.publisher_2 = self.create_publisher(String, 'robot_teclas', 10)  # 
+        timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.last_key = None
 
-    def on_press(self, key):
+    def on_press(self,key):
 
-        msg = Twist()    
+        msg_cmdVel = Twist()  
+        msg_tecla = String()  
 
         try:      
 
-            if key.char == "w": #adelante
-                msg.linear.x = float(lin)
-                msg.linear.z = 0.0
-                msg.angular.z = 0.0
-                self.publisher_.publish(msg)
-                self.get_logger().info('Adelante')
+            if key.char == "a": #adelante
+
+                msg_tecla.data = "a"
+                msg_cmdVel.linear.x = -float(lin)
+                msg_cmdVel.linear.z = 0.0
+                msg_cmdVel.angular.z = 0.0
+                self.publisher_1.publish(msg_cmdVel)
+                self.get_logger().info('Izquierda')
+                self.publisher_2.publish(msg_tecla)
+                self.get_logger().info('Izquierda publicada')
                 self.i += 1        
 
-            elif key.char == "s": #atras
+            elif key.char == "d": #atras
 
-                msg.linear.x = -float(lin)
-                msg.linear.z = 0.0
-                msg.angular.z = 0.0
-                self.publisher_.publish(msg)
-                self.get_logger().info('Atras')
+                msg_tecla.data = "d"
+                msg_cmdVel.linear.x = float(lin)
+                msg_cmdVel.linear.z = 0.0
+                msg_cmdVel.angular.z = 0.0
+                self.publisher_1.publish(msg_cmdVel)
+                self.get_logger().info('Derecha')
+                self.publisher_2.publish(msg_tecla)
+                self.get_logger().info('Derecha publicada')
                 self.i += 1
+                
+            elif key.char == "l": #giro derecha
 
-            elif key.char == "d": #giro derecha
-
-                msg.linear.x = 0.0
-                msg.linear.z = 0.0
-                msg.angular.z = -float(ang)
-                self.publisher_.publish(msg)
+                msg_tecla.data = "l"
+                msg_cmdVel.linear.x = 0.0
+                msg_cmdVel.linear.z = 0.0
+                msg_cmdVel.angular.z = float(ang)
+                self.publisher_1.publish(msg_cmdVel)
                 self.get_logger().info('Giro derecha')
+                self.publisher_2.publish(msg_tecla)
+                self.get_logger().info('Giro derecha publicada')
                 self.i += 1
 
-            elif key.char == "a": #giro izquierda
+            elif key.char == "k": #giro izquierda
 
-                msg.linear.x = 0.0
-                msg.linear.z = 0.0
-                msg.angular.z = float(ang)
-                self.publisher_.publish(msg)
+                msg_tecla.data = "k"
+                msg_cmdVel.linear.x = 0.0
+                msg_cmdVel.linear.z = 0.0
+                msg_cmdVel.angular.z = -float(ang)
+                self.publisher_1.publish(msg_cmdVel)
                 self.get_logger().info('Giro izquierda')
+                self.publisher_2.publish(msg_tecla)
+                self.get_logger().info('Giro izquierda publicada')
                 self.i += 1
 
         except: 
@@ -66,21 +82,15 @@ class RobotTeleop(Node):
 
     def on_release(self, key):
 
-        msg = Twist()
-        msg.linear.x = 0.0
-        msg.linear.z = 0.0
-        msg.angular.z = 0.0
-        self.publisher_.publish(msg)
+        msg_cmdVel = Twist()
+        msg_cmdVel.linear.x = 0.0
+        msg_cmdVel.linear.z = 0.0
+        msg_cmdVel.angular.z = 0.0
+        self.publisher_1.publish(msg_cmdVel)
         self.get_logger().info('Stop')
-        self.i += 1
+        self.i += 1               
 
-    def timer_callback(self):
-        with keyboard.Listener(
-                on_press=self.on_press,
-                on_release=self.on_release) as listener:
-            listener.join()               
-
-    def timer_callback(self):
+    def timer_callback(self):   #Matar esto y dejar el listener en el init?
         with keyboard.Listener(
                 on_press=self.on_press,
                 on_release=self.on_release) as listener:
@@ -90,13 +100,24 @@ class RobotTeleop(Node):
 def main(args=None):
 
     rclpy.init(args=args)
-    robot_teleop = RobotTeleop()
+    robot_teleop = Robot_teleop()
     rclpy.spin(robot_teleop)
     # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
+   # (optional - otherwise it will be done automatically
+   # when the garbage collector destroys the node object)
     robot_teleop.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
+
+
+
+
+
